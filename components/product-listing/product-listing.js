7b479614c4e15fe98e4b255c535d7127484bac99
Rapteon <rapteon@tutanota.com>
2026-03-05 09:46:59 +0530

    Removed script tag within product page. Script tags for components required by a parent are the parent component's responsibility.

--- components/product-listing/product-listing.js
+++ components/product-listing/product-listing.js
@@ -10,6 +10,14 @@ class ProductListing extends HTMLElement {
     const shadow = this.attachShadow({ mode: "open" });
     const div = document.createElement("div");
 
+    // Add script tags for components used by this class.
+    let uses = ["/components/product-listing/product-images/product-images.js"]
+    for (let use of uses) {
+      const script = document.createElement('script');
+      script.src = use;
+      shadow.appendChild(script);
+    }
+    
     // Fetch template
     // Can be refactored later into a function which appends
     // a noed by fetching innerHTML from a template at a specific path
